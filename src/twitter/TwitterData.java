e251b1671fb7cc03907425bb658a75c6e24b1e84
Jack Musial <jack@musi.al>
2016-05-27 19:40:13 +0100

    addfiles

---
+++ src/twitter/TwitterData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package twitter;
+
+/**
+ *
+ * @author jackmusial
+ */
+public final class TwitterData {
+    public static final String CONSUMER_KEY = "EgzK0RfsxfSiOIuaocz1dWl2i";
+    public static final String CONSUMER_SECRET = "mv2MSuA2AsQXzLoyyJCSm2e1WUlKcgw5VIHkkhI6xTrbxV2t7t";
+    public static final String ACCESS_TOKEN = "725253533071712256-l1F54xJlqmtlCfzZ9gN7Mgd09C5YFgg";
+    public static final String ACCESS_TOKEN_SECRET = "Kw7qX4XmXtXdtujBmWD8wA54myr9d8vBPUihRxBDn3bWX";
+    public static final String OAUTH_TOKEN_URL = "https://api.twitter.com/oauth2/token";
+    public static final String RATE_LIMIT_STATUS_URL = "https://api.twitter.com/1.1/application/rate_limit_status.json?" 
+            + "resources=search";
+    public static final String SEARCH_URL = "https://api.twitter.com/1.1/search/tweets.json?q=";
+}
